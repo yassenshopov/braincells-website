fd2ee1fc710063b598d8cefb00be70fbeb4abfce
Yassen Shopov <yassen.shopov@nuvei.com>
2024-06-18 21:47:07 +0300

    chore: Update FAQSection component imports to use single quotes

--- src/app/components/faq-section.js
+++ src/app/components/faq-section.js
@@ -1,8 +1,8 @@
-import home from "../styles/css/home.module.css";
+import home from '../styles/css/home.module.css';
 
 export default function FAQSection() {
   return (
-    <section className={home.faqSection + " " + home.darkBg}>
+    <section className={home.faqSection + ' ' + home.darkBg}>
       <h2>FAQs</h2>
       <h3>
         Most common questions about the platform answered here.<br></br>If you
@@ -10,37 +10,37 @@ export default function FAQSection() {
       </h3>
       <div className={home.faqSectionGrid}>
         <div>
-          <h3><img src="https://www.notion.so/icons/question-mark_gray.svg?mode=light" alt="Question mark icon" />
+          <h3><img src='https://www.notion.so/icons/question-mark_gray.svg?mode=light' alt='Question mark icon' />
             What apps does braincells.io work with?</h3>
           <p>
-            Braincells.io integrates with your{" "}
-            <a href="https://notion.so/product">Notion</a> workspace and lets it
+            Braincells.io integrates with your{' '}
+            <a href='https://notion.so/product'>Notion</a> workspace and lets it
             communicate with Slack, Google Sheets, and more.
           </p>
         </div>
         <div>
-          <h3><img src="https://www.notion.so/icons/question-mark_gray.svg?mode=light" alt="Question mark icon" />Is there a free tier?</h3>
+          <h3><img src='https://www.notion.so/icons/question-mark_gray.svg?mode=light' alt='Question mark icon' />Is there a free tier?</h3>
           <p>
-            Yes, we offer a free tier with a limit of 1 active "braincell". You
+            Yes, we offer a free tier with a limit of 1 active 'braincell'. You
             can upgrade to a paid plan at any time.
           </p>
         </div>
         <div>
-          <h3><img src="https://www.notion.so/icons/question-mark_gray.svg?mode=light" alt="Question mark icon" />Is there a risk for my Notion content?</h3>
+          <h3><img src='https://www.notion.so/icons/question-mark_gray.svg?mode=light' alt='Question mark icon' />Is there a risk for my Notion content?</h3>
           <p>
             Braincells.io only reads and writes to your Notion workspace. We
             don't store any of your data on our servers.
           </p>
         </div>
         <div>
-          <h3><img src="https://www.notion.so/icons/question-mark_gray.svg?mode=light" alt="Question mark icon" />Do you offer onboarding assistance for new users?</h3>
+          <h3><img src='https://www.notion.so/icons/question-mark_gray.svg?mode=light' alt='Question mark icon' />Do you offer onboarding assistance for new users?</h3>
           <p>
             Yes, we offer a 30-minute onboarding session for new users. You can
             book a session after you sign up.
           </p>
         </div>
         <div>
-          <h3><img src="https://www.notion.so/icons/question-mark_gray.svg?mode=light" alt="Question mark icon" />What if I need help with a specific automation?</h3>
+          <h3><img src='https://www.notion.so/icons/question-mark_gray.svg?mode=light' alt='Question mark icon' />What if I need help with a specific automation?</h3>
           <p>
             Customisation is key to our platform. If you have a specific need
             that isn't covered by our current automations, we can create a
@@ -54,7 +54,7 @@ export default function FAQSection() {
           For any other questions, feel free to contact our team, and we'll get
           back to you as soon as possible.
         </p>
-        <a href="/contact-us" className={home.faqSectionContactButton}>
+        <a href='/contact-us' className={home.faqSectionContactButton}>
           Contact us
         </a>
       </div>
